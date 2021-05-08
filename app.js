8dbc3e6eaa8d9b30705094a833e142a154b34cd6
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-05-08 15:19:21 -0500

    Added the ability for the server to update the JSON file from the /edit/ page. Now quiz entries can be made through GUI

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
+
+app.use(bodyParser.urlencoded({ extended: true  }));
 
 app.get('/edit', (req, res) => {
     res.sendFile(__dirname + '/edit.html');
@@ -11,5 +15,31 @@ app.get('/edit', (req, res) => {
 
 app.use('/public', express.static(__dirname + '/public'));
 
+app.post('/', (req, res) => {
+    console.log('Got body', req.body);
+    addToQuiz(req.body);
+    res.sendStatus(200);
+    res.sendFile(__dirname + '/edit.html');
+});
 
 app.listen(8000);
+
+function addToQuiz(addition) {
+    let list = JSON.parse(fs.readFileSync(__dirname + '/public/list.json'));
+
+    let entry = {
+        "prompt": addition.prompt,
+        "answers": [
+            addition.answer1,
+            addition.answer2,
+            addition.answer3,
+            addition.answer4
+        ],
+        "correct": addition.correct,
+        "message": addition.message
+    };
+
+    list.quiz.push(entry);
+
+    fs.writeFileSync(__dirname + '/public/list.json', JSON.stringify(list));
+}
